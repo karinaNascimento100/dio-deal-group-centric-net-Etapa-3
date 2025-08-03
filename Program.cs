3dc34a2c6b5dd27c41258e42a45d9a49100928d8
karinaNascimento100! <kalomeira9@gmail.com>
2025-08-03 14:36:26 -0300

    Tratando uma exceção com inclusão de mensagem.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
 ﻿using System.Net.NetworkInformation;
 using dio_deal_group_centric_net_Etapa_2.Models;
 
-
 Pessoa p1 = new Pessoa { Nome = "Karina", Sobrenome = "Nascimento", Idade = 51 };
 p1.Apresentar();
 
 Pessoa p2 = new Pessoa { Nome = "João", Sobrenome = "Silva" };
-
-
 
 Curso cursoDeProgramacao = new Curso { Nome = "Curso de Programação" };
 
@@ -15,8 +12,15 @@ cursoDeProgramacao.AdicionarAluno(p1);
 cursoDeProgramacao.AdicionarAluno(p2);
 cursoDeProgramacao.ListarAlunos();
 
-string[] linhas = File.ReadAllLines("Models/Arquivos/arquivoLeitura.txt");
-foreach (string linha in linhas)
+try
 {
-    Console.WriteLine(linha);
+    string[] linhas = File.ReadAllLines("Models/Arquivos/arquivoLeitura.txt");
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro favor analisar o seguinte aspecto: {ex.Message}");
 }
